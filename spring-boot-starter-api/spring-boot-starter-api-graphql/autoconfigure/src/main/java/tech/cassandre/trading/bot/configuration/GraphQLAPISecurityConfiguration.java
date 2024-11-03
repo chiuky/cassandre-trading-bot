6d40cc8c158099a49009940b48936a84e2dc56de
Edgar <edgar.vahitov@gmail.com>
2024-11-03 13:54:50 +0200

    fix tests

--- spring-boot-starter-api/spring-boot-starter-api-graphql/autoconfigure/src/main/java/tech/cassandre/trading/bot/configuration/GraphQLAPISecurityConfiguration.java
+++ spring-boot-starter-api/spring-boot-starter-api-graphql/autoconfigure/src/main/java/tech/cassandre/trading/bot/configuration/GraphQLAPISecurityConfiguration.java
@@ -1,29 +1,31 @@
 package tech.cassandre.trading.bot.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 /**
  * GraphQL API security configuration.
  */
+@SuppressWarnings("DesignForExtension")
 @Configuration
-@EnableWebSecurity
-@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE + 1)
-public final class GraphQLAPISecurityConfiguration {
+public class GraphQLAPISecurityConfiguration {
 
-    /** API Key. */
+    /**
+     * API Key.
+     */
     @Value("${cassandre.trading.bot.api.graphql.key:}")
     private String key;
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE + 1)
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
         if (key == null || key.isBlank()) {
             // if key is not set, no security at all. Everything is accessible.
@@ -36,7 +38,6 @@ public final class GraphQLAPISecurityConfiguration {
             // If a key is set, we ask for key on every request (X-API-Key header).
             GraphQLAPIKeyAuthenticationFilter filter = new GraphQLAPIKeyAuthenticationFilter();
             filter.setAuthenticationManager(new GraphQLAPIKeyAuthenticationManager(key));
-
             http
                     .securityMatcher("/graphql/**")
                     .csrf(AbstractHttpConfigurer::disable)
@@ -44,7 +45,6 @@ public final class GraphQLAPISecurityConfiguration {
                     .addFilter(filter)
                     .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
         }
-
         return http.build();
     }
 }
